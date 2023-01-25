6fa128f1fe77ec03ef50975ebd9e21e6c21960bb
Satyam Pratap <satyampratap007@gmail.com>
2023-01-25 23:47:32 +0530

    representation

--- Trees/binary_tree.cpp/representation.cpp
+++ Trees/binary_tree.cpp/representation.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+struct node
+{
+    public:
+    int data;
+    struct node*left;
+    struct node*right;
+
+    node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int main()
+{
+    struct node * root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->right = new node(5);
 
 }
