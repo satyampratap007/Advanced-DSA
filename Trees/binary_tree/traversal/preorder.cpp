20fd379c0791bfd6107a441032d0a1ae089b12e2
Satyam Pratap <satyampratap007@gmail.com>
2023-01-29 22:15:37 +0530

    iterative

--- Trees/binary_tree/traversal/preorder.cpp
+++ Trees/binary_tree/traversal/preorder.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct node
@@ -27,8 +28,30 @@ void preorder(node *root){
     preorder(root->right);
 }
 
+// Iteratitve preorder traversal
 
-
+void iterative_preorder(node *root){
+    if (root == NULL)
+    {
+        return;
+    }
+    stack <node*> s;
+    s.push(root);
+    while (!s.empty())
+    {
+        node* temp = s.top();
+        s.pop();
+        cout << temp->data << " ";
+        if (temp->right != NULL)
+        {
+            s.push(temp->right);
+        }
+        if (temp->left != NULL)
+        {
+            s.push(temp->left);
+        }
+    }
+}
 int main()
 {
     struct node * root = new node(1);
@@ -37,6 +60,8 @@ int main()
     root->left->right = new node(5);
     cout << "\n Preorder traversal of given B.T: ";
     preorder(root);
+    cout << "\n\n\n\n";
+    iterative_preorder(root);
 }
 
 
