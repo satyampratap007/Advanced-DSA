11ee98852ee03b0934cc2198c962f7b3dbe3f3d5
Satyam Pratap <satyampratap007@gmail.com>
2023-01-30 18:19:58 +0530

    using2 stack

--- Trees/binary_tree/traversal/postorder.cpp
+++ Trees/binary_tree/traversal/postorder.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct node
@@ -26,6 +26,38 @@ void postorder(node *root){
     cout << root->data << " ";
 }
 
+// Iterative solution - Using 2 stacks
+
+void iterative_postorder(node*root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    stack <node*> st1;
+    stack <node*> st2;
+    st1.push(root);
+    while (!st1.empty())
+    {
+        root = st1.top();
+        st1.pop();
+        st2.push(root);
+        if (root->left != NULL)
+        {
+            st1.push(root->left);
+        }
+        if (root->right != NULL)
+        {
+            st1.push(root->right);
+        }
+    }
+    while (!st2.empty())
+    {
+        cout << st2.top()->data << " ";
+        st2.pop();
+    }
+}
+
 int main()
 {
     struct node * root = new node(1);
@@ -33,8 +65,12 @@ int main()
     root->right = new node(3);
     root->left->right = new node(5);
     cout << "\n Preorder traversal of given B.T: ";
-    preorder(root);
+    postorder(root);
+    cout << "\n\n\n";
+    iterative_postorder(root);
+    return 0;
 }
+
 
 
 
