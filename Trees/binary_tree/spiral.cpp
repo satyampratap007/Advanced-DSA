bb24e356e5d2508ffd320046e54cd74b37257400
Satyam Pratap <satyampratap007@gmail.com>
2023-02-21 14:13:32 +0530

    spiral printing using single queue

--- Trees/binary_tree/spiral.cpp
+++ Trees/binary_tree/spiral.cpp
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+using namespace std;
+
+//  * Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -18,7 +19,6 @@ public:
         queue <TreeNode*> q;
         q.push(root);
         bool flag = true;
-
         while(!q.empty())
         {
             int len = q.size();
