8b4f402b14f221b1b598209526e96e421b9569dd
Satyam Pratap <satyampratap007@gmail.com>
2023-03-23 00:14:04 +0530

    ways of creating 2d array

--- .vscode/Multidimensional_Array/creation.cpp
+++ .vscode/Multidimensional_Array/creation.cpp
@@ -36,11 +36,37 @@ int main(){
     
     /* 3) Using array of vectors */
 
-    
+    // vector <int> arr[m];
+    // for (int i = 0; i < m; i++)
+    // {
+    //     for (int j{0}; j < n; j++)
+    //     {
+    //         arr[i].push_back(10);
+    //         cout << arr[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
 
-      
+    /* 4) Using vector of vectors */
+    vector <vector <int>> arr;
+    for (int i = 0; i < m; i++)
+    {
+        vector <int> v;
+        for (int i = 0; i < n; i++)
+        {
+            v.push_back(10);
+        }
+        arr.push_back(v);
+    }
 
-
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 
 }
