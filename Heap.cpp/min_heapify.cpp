7a808d962fbbfa65995df29cc3493e1e727ca08c
Satyam Pratap <satyampratap007@gmail.com>
2023-10-16 03:15:40 +0530

    min_heapify

---
+++ Heap.cpp/min_heapify.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+class MinHeap{
+    int size;
+    int capacity;
+    int *arr;
+
+    MinHeap(int c)
+    {
+        size = 0;
+        arr = new int(c);
+        capacity = c;
+    }
+
+    int left(int i){
+        return 2*i + 1;
+    }
+    int right(int i){
+        return 2*i + 2;
+    }
+    int parent(int i){
+        return (i-1)/2;
+    }
+
+    void min_heapify(int ind)
+    {
+        int l = left(ind);
+        int r = right(ind);
+        
+        int mini = ind;
+        if(ind < size && arr[l] < arr[ind]){
+            mini = l;
+        }
+        if(ind < size && arr[r] < arr[ind]){
+            mini = r;
+        }
+        if(mini != ind)
+        {
+            swap(arr[ind], arr[mini]);
+            min_heapify(mini);
+        }
+
+    }
+
+
+
+};
