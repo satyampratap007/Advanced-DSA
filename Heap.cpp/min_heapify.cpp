60a77836d5297f34c778a58dff2cf70954866855
Satyam Pratap <satyampratap007@gmail.com>
2023-10-16 03:36:10 +0530

    extract_minimum

--- Heap.cpp/min_heapify.cpp
+++ Heap.cpp/min_heapify.cpp
@@ -44,6 +44,21 @@ class MinHeap{
 
     }
 
+    int extract_min(){
+        if(size == 0)   return INT_MAX;
+        if(size == 1){
+            size--;
+            return arr[0];
+        }    
+
+        swap(arr[0], arr[size-1]);
+        size--;
+        min_heapify(0);
+        return arr[size];
+    }
+
+
+
 
 
 };
