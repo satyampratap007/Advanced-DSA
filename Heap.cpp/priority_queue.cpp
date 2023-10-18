04e6b263a55f9dd2607e90298ec2fad97580482d
Satyam Pratap <satyampratap007@gmail.com>
2023-10-19 03:53:46 +0530

    customize_pq

--- Heap.cpp/priority_queue.cpp
+++ Heap.cpp/priority_queue.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
-struct Person{
-    int ht;
-    int age;
-    Person();
-    Person(int a, int h){
-        age = a;
-        ht = h;
+
+struct Person {
+    double ht;
+    double age;
+    Person() {}
+    Person(double a, double h) : age(a), ht(h) {}
+};
+
+struct my_cmp {
+    bool operator()(const Person& p1, const Person& p2) {
+        // return p1.ht > p2.ht; // Compare based on height
+        // return p1.age > p2.age; // Compare based on age
     }
 };
 
-int main(){
+int main() {
+    // <age, height>
+    vector<Person> vec = {{60, 5.5}, {50, 6.0}, {70, 6.5}, {80, 6.7}, {40, 4.0}};
 
+    priority_queue<Person, vector<Person>, my_cmp> pq(vec.begin(), vec.end());
+
+    while (!pq.empty()) {
+        Person p = pq.top();
+        cout << "Age: " << p.age << ", Height: " << p.ht << endl;
+        pq.pop();
+    }
+
+    return 0;
 }
