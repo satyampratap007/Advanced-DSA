b87bd2d8a31ab1db9d9490ac8e7b7dc532cda922
Satyam Pratap <satyampratap007@gmail.com>
2023-10-19 03:00:31 +0530

    min_heap_insertion

---
+++ Heap.cpp/priority_queue.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+struct Person{
+    int ht;
+    int age;
+    Person();
+    Person(int a, int h){
+        age = a;
+        ht = h;
+    }
+};
+
+int main(){
+
+}
